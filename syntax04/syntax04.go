da12b4528775870bd7c9a0b49061151b14557be0
9jabis <kjb990401@naver.com>
2021-09-14 02:20:51 +0900

    v0.6

--- syntax04/syntax04.go
+++ syntax04/syntax04.go
@@ -6,7 +6,7 @@ import (
 	"time"      // seed 생성용 패키지
 )
 
-//	난수 추출된 수의 소수 판정 프로그램 v0.4
+//	난수 추출된 수의 소수 판정 프로그램 v0.6
 //	소수 : 1과 자기자신외에는 나누어 떨어지지 않는 수 (0과 1은 제외)
 
 func main() {
@@ -20,8 +20,9 @@ func main() {
 	for i := 2; i < number; i++ { // 1과 number일 때 loop  돌지 않음
 		if number%i == 0 {
 			isPrime = false
+			break //  첫 번째 약수가 발견되면 반복문 즉시 종료
 		}
-		fmt.Println(i, " ")
+		//fmt.Print(i, " ")
 	}
 
 	if isPrime { // 비교 연산자 제거
